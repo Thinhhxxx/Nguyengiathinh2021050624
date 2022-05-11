22602cb309612a932b5f1ba70978ca727f10d537
Thinhhxxx <105273580+Thinhhxxx@users.noreply.github.com>
2022-05-11 17:40:37 +0700

    Add files via upload

---
+++ main.java.java
@@ -0,0 +1,16 @@
+package nguyengiathinh_2021050624;
+
+public class main {
+    public static void main(String[]args) {
+        staff A = new staff();
+        A.setStaff();
+        A.getBonus();
+        A.getStaff();
+        parttimeStaff B = new parttimeStaff();
+        B.setStaff();
+        B.getBonus();
+        B.getStaff();
+
+    }
+}
+
---
+++ parttimeStaff.java.java
@@ -0,0 +1,21 @@
+package nguyengiathinh_2021050624;
+import java.util.Scanner;
+public class parttimeStaff extends staff {
+   
+    protected String Name;
+    protected int Salary,Bonus;
+    public void getBonus(){
+        Bonus = 0;
+
+    }
+     void setStaff(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap ten nhan vien: ");
+        Name = sc.nextLine();
+        System.out.println("Nhap luong cho nhan vien: ");
+        Salary = sc.nextInt();
+    }
+    void getStaff(){
+        System.out.println("Ho va ten: "+Name+"\nLuong cung:"+Salary+"\nThuong them: "+Bonus+"\nTong luong: "+(Salary+Bonus));
+    }
+}
